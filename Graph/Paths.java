5c7d0341640fb2f8338a8353b07abfe3d6a3190c
Ritik <ritik.cse18@chitkarauniversity.edu.in>
2020-05-12 23:24:18 +0530

    Bellman-Ford Algorithm • The Bellman-Ford algorithm: 	-> Finds shortest paths in weighted graphs with negative edge weights 	-> runs in time O(nm) on a graph G with n vertices and m edges. 	-> runs in time O(n^3) in complete graph as E=(n*(n-1))/2 in complete graph and V= n
    • If there are no negative cycles in G:
    	-> the BF algorithm terminates with d (n-1) [v] = d(s,v).
    • If there are negative cycles in G:
    	-> the BF algorithm returns negative cycle.

--- Graph/Paths.java
+++ Graph/Paths.java
@@ -46,16 +46,43 @@ public class Paths{
 			}
 		}
 	}
+	static void BellmanFordAlgo(HashMap<Integer,ArrayList<Integer>> h,HashMap<String,Integer> weights,HashMap<Integer,Integer> dist,int src,HashSet<Integer> Vertices)
+	{
+		for(Integer i:Vertices)
+			dist.put(i,Integer.MAX_VALUE);
+		dist.put(src,0);
+		for(Integer i:Vertices)
+		{
+			for(Map.Entry<Integer,ArrayList<Integer>> m:h.entrySet())
+			{int u=m.getKey();
+				for(Integer v:m.getValue())
+				{
+			//		System.out.println(dist.get(v)+" "+dist.get(u)+" "+weights.get(""+u+" "+v));
+					dist.put(v,Math.min(dist.get(v),dist.get(u)+weights.get(""+u+" "+v)));
+				}
+			}
+		}
+		/**Here Below 
+		 * distance mapping of every i is maintained and when n-1 != n 
+		 * it means it have negative weights cylce so it will keep on changing
+		 * <--You should change function return type and other code in this function 
+		 *  in order to determine the negative cycle-->
+		//if(dist[n-1]!=dist[n]) return "NEGATIVE CYCLE"**/
+	
+	}
 	public static void main(String[] args)
 	{
 		Scanner scan=new Scanner(System.in);
 		HashMap<Integer,ArrayList<Integer>> h=new HashMap<>();
 		HashMap<String,Integer> weights=new HashMap<>();
+		HashSet<Integer> Vertices=new HashSet<>();
 		while(true)
 		{
 			int x=scan.nextInt(),y=scan.nextInt();
 			if(x==-1||y==-1)
 				break;
+			Vertices.add(x);
+			Vertices.add(y);
 			if(h.containsKey(x))
 			{
 				ArrayList<Integer> l=h.get(x);
@@ -71,9 +98,15 @@ public class Paths{
 			String key=x+" "+y;
 			weights.put(key,z);
 		}
+		System.out.println("Data: "+h);
+		System.out.println("Weights: "+weights);
+		int choice=2;//1 dijkstra 2 bellman ford algo
 		int src=scan.nextInt();//src from were to path is need to be measure
 		HashMap<Integer,Integer> dist=new HashMap<>();
+		if(choice==1)
 		dijkstra(h,weights,dist,src);
+		else 
+			BellmanFordAlgo(h,weights,dist,src,Vertices);
 	//	bfsPath(h,src,dist);
 		int dest=scan.nextInt();//to where dist needs to be measured
 		System.out.println(dist);
